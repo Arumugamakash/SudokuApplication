29b55a46fdd052c37128c23b3c2bd784847948e3
Arumugamakash <142109407+Arumugamakash@users.noreply.github.com>
2024-04-21 02:21:27 +0530

    Update SudokuSolver.java

--- src/com/arumugamakash/sudokuapplication/SudokuSolver.java
+++ src/com/arumugamakash/sudokuapplication/SudokuSolver.java
@@ -9,7 +9,7 @@ public class SudokuSolver {
 		int[][] board = { { 7, 0, 2, 0, 5, 0, 6, 0, 0 }, { 0, 0, 0, 0, 0, 3, 0, 0, 0 }, { 1, 0, 0, 0, 0, 9, 5, 0, 0 },
 				{ 8, 0, 0, 0, 0, 0, 0, 9, 0 }, { 0, 4, 3, 0, 0, 0, 7, 5, 0 }, { 0, 9, 0, 0, 0, 0, 0, 0, 8 },
 				{ 0, 0, 9, 7, 0, 0, 0, 0, 5 }, { 0, 0, 0, 2, 0, 0, 0, 0, 0 }, { 0, 0, 7, 0, 4, 0, 2, 0, 3 } };
-		//////
+		System.out.println("our Input is...\n");
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
 				System.out.print(board[i][j] + " ");
@@ -17,7 +17,8 @@ public class SudokuSolver {
 			System.out.println();
 		}
 		inputNumber(board);
-		System.out.println("Solved Successfully");
+		System.out.println("\nSolved Successfully");
+		System.out.println("our output is...\n");
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
 				System.out.print(board[i][j] + " ");
@@ -33,7 +34,7 @@ public class SudokuSolver {
 				if (board[i][j] == 0) {
 					int row = i;
 					int column = j;
-					System.out.println("Enter the validInput");
+					System.out.println("Enter the " + row + " Row and " + column + " column");
 					int input = sc.nextInt();//
 					sc.nextLine();
 					if (checkRow(board, input, row)) {
